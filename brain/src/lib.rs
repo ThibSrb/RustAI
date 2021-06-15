a632e325a3173e31e467cdd836e298ef273a4597
thibault.serabian <thibault.serabian@epita.fr>
2021-06-15 12:07:07 +0200

    Reset and new init commit

---
+++ brain/src/lib.rs
@@ -0,0 +1,345 @@
+use graph::*;
+use rand::Rng;
+
+//SAVEFILE
+extern crate savefile;
+use savefile::prelude::*;
+#[macro_use]
+extern crate savefile_derive;
+
+#[derive(Debug, Clone, Savefile)]
+pub struct Brain {
+    weights: Vec<f64>,
+    values: Vec<f64>,
+    network: Graph,
+    pub layersizes: Vec<usize>,
+    nbin: usize,
+    nbout: usize,
+    pub activation_function: ActivationFunction,
+}
+
+#[derive(Debug, Clone, Savefile, Copy)]
+pub enum ActivationFunction {
+    Sigmoide,
+    Heaviside,
+    Indentity,
+}
+
+impl Brain {
+    pub fn new_basic(
+        nbin: usize,
+        nbout: usize,
+        width: usize,
+        depth: usize,
+        activation_function: ActivationFunction,
+    ) -> Brain {
+        let mut rng = rand::thread_rng();
+        let nb_verrtex = nbin + nbout + (width * (depth - 2));
+        let mut res: Brain = Brain {
+            network: Graph::new(nb_verrtex, false, false),
+            weights: vec![0.0; nb_verrtex],
+            values: vec![0.0; nb_verrtex],
+            layersizes: vec![0; depth],
+            nbin: nbin,
+            nbout: nbout,
+            activation_function: activation_function,
+        };
+        for i in 1..(depth - 1) {
+            res.layersizes[i] = width;
+        }
+        res.layersizes[0] = nbin;
+        res.layersizes[depth - 1] = nbout;
+
+        let mut sl = 0; // start of layer
+        let mut snl = nbin; // start of next layer
+        let mut snnl = nbin + res.layersizes[1];
+        for ls in 0..(depth - 1) {
+            for n in sl..snl {
+                for nn in snl..snnl {
+                    res.network.add_edge(n, nn);
+                    res.network
+                        .set_cost((n, nn), (rng.gen::<f64>() * 2.0) - 1.0);
+                    //res.network.add_edge(nn, n);
+                }
+            }
+            sl += res.layersizes[ls];
+            snl += res.layersizes[ls + 1];
+            if ls + 2 < depth {
+                snnl += res.layersizes[ls + 2];
+            }
+        }
+        res
+    }
+
+    pub fn to_dot(&mut self) -> String {
+        let mut labels: Vec<String> = vec![];
+        for i in self.weights.iter() {
+            labels.push((*i).to_string());
+        }
+        //self.network.to_dot_labeled(labels)
+        self.network.to_dot()
+    }
+
+    pub fn heaviside(x: f64) -> f64 {
+        match x {
+            y if y <= 0.0 => 0.0,
+            _ => 1.0,
+        }
+    }
+
+    pub fn sigmoide(x: f64) -> f64 {
+        1.0 / (1.0 + (-5.0 * x).exp())
+    }
+
+    pub fn compute(&mut self, entry: &Vec<f64>) -> Vec<f64> {
+        let mut res = Vec::new();
+        if self.nbin == entry.len() {
+            let mut traversal = vec![];
+            let mut mark = vec![false; self.network.order];
+
+            //let max = Brain::max_vf64(&entry);
+            //let min = Brain::min_vf64(&entry);
+            for (i, val) in entry.iter().enumerate() {
+                self.values[i] = *val;
+                traversal.push(i);
+                mark[i] = true;
+            }
+
+            while !traversal.is_empty() {
+                let cur = traversal[0];
+                traversal.remove(0);
+                if cur < self.network.order - self.nbout {
+                    self.values[cur] += self.weights[cur];
+                    self.values[cur] = self.values[cur];
+                    self.values[cur] = match self.activation_function {
+                        ActivationFunction::Heaviside => Brain::heaviside(self.values[cur]),
+                        ActivationFunction::Indentity => self.values[cur],
+                        _ => Brain::sigmoide(self.values[cur]),
+                    };
+
+                    for i in self.network.adjlist[cur].iter() {
+                        self.values[*i] += self.values[cur] * self.network.get_cost((cur, *i));
+                        if !mark[*i] {
+                            mark[*i] = true;
+                            traversal.push(*i);
+                        }
+                    }
+                } else {
+                    self.values[cur] += self.weights[cur];
+                }
+            }
+            for (i, _j) in (0..self.network.order).rev().zip(0..self.nbout) {
+                res.push(self.values[i]);
+            }
+        } else {
+            eprintln!("Uncompatible entry");
+        }
+        self.values.iter_mut().for_each(|x| *x = 0.0); //resets all neurones values
+        res.iter().rev().map(|x| f64::abs(*x)).collect()
+    }
+
+    pub fn apply_mutation(&mut self, prob: f64, factor: f64) {
+        let mut rng = rand::thread_rng();
+        let mut alea: f64;
+        let mut mutation: f64;
+        let mut mark = vec![false; self.network.order];
+        for i in 0..self.network.order {
+            mark[i] = true;
+            alea = rng.gen::<f64>();
+            if alea <= prob {
+                mutation = (rng.gen::<f64>() * 2.0 - 1.0) * factor;
+                self.weights[i] += mutation;
+                for j in 0..self.network.adjlist[i].len() {
+                    if !mark[j] {
+                        alea = rng.gen::<f64>();
+                        if alea <= prob {
+                            mutation = (rng.gen::<f64>() * 2.0 - 1.0) * factor;
+                            self.network
+                                .set_cost((i, j), self.network.get_cost((i, j)) + mutation);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn genetic_selection(
+        nbin: usize,
+        nbout: usize,
+        width: usize,
+        depth: usize,
+        activation_function: ActivationFunction,
+        compete_function: fn(&mut Vec<Brain>) -> Brain,
+        nb_gen: usize,
+        gen_size: usize,
+        mutation_prob: f64,
+        mutation_fac: f64,
+    ) -> Brain {
+        let mut gen = vec![];
+        for _u in 0..gen_size {
+            gen.push(Brain::new_basic(
+                nbin,
+                nbout,
+                width,
+                depth,
+                activation_function,
+            ));
+        }
+        for _i in 0..nb_gen {
+            let best: Brain = compete_function(&mut gen);
+            gen[0] = best.clone();
+            for j in 1..gen_size {
+                let mut wi = best.clone();
+                wi.apply_mutation(mutation_prob, mutation_fac);
+                gen[j] = wi;
+            }
+        }
+        let best: Brain = compete_function(&mut gen);
+        return best;
+    }
+
+    pub fn learn_xor(pool: &mut Vec<Brain>) -> Brain {
+        //let mut xorer = Brain::genetic_selection(2, 1, 4, 3, Brain::learn_xor, 500, 5000, 0.4, 0.4); simple
+        //let mut xorer = Brain::genetic_selection(2, 1, 5, 5, Brain::learn_xor, 500, 5000, 0.4, 0.4); more efficient but need a greater time of learn
+        let mut error_values = vec![0.0; pool.len()];
+        for (i, brain) in pool.iter_mut().enumerate() {
+            let r1 = brain.compute(&vec![0.0, 0.0]);
+            let r2 = brain.compute(&vec![0.0, 1.0]);
+            let r3 = brain.compute(&vec![1.0, 0.0]);
+            let r4 = brain.compute(&vec![1.0, 1.0]);
+
+            error_values[i] = f64::abs(0.0 - r1[0])
+                + f64::abs(1.0 - r2[0])
+                + f64::abs(1.0 - r3[0])
+                + f64::abs(0.0 - r4[0]);
+        }
+        //get the minimum error
+        let mut min = -1.0;
+        let mut bi = 0;
+        for (i, j) in error_values.iter().enumerate() {
+            if *j < min || min == -1.0 {
+                min = *j;
+                bi = i;
+            }
+        }
+        pool[bi].clone()
+    }
+
+    pub fn learn_or(pool: &mut Vec<Brain>) -> Brain {
+        //let mut xorer = Brain::genetic_selection(2, 1, 4, 3, Brain::learn_xor, 500, 5000, 0.4, 0.4); simple
+        //let mut xorer = Brain::genetic_selection(2, 1, 5, 5, Brain::learn_xor, 500, 5000, 0.4, 0.4); more efficient but need a greater time of learn
+        let mut error_values = vec![0.0; pool.len()];
+        for (i, brain) in pool.iter_mut().enumerate() {
+            let r1 = brain.compute(&vec![0.0, 0.0]);
+            let r2 = brain.compute(&vec![0.0, 1.0]);
+            let r3 = brain.compute(&vec![1.0, 0.0]);
+            let r4 = brain.compute(&vec![1.0, 1.0]);
+
+            error_values[i] = f64::abs(0.0 - r1[0])
+                + f64::abs(1.0 - r2[0])
+                + f64::abs(1.0 - r3[0])
+                + f64::abs(1.0 - r4[0]);
+        }
+        //get the minimum error
+        let mut min = -1.0;
+        let mut bi = 0;
+        for (i, j) in error_values.iter().enumerate() {
+            if *j < min || min == -1.0 {
+                min = *j;
+                bi = i;
+            }
+        }
+        pool[bi].clone()
+    }
+
+    pub fn learn_sortition(pool: &mut Vec<Brain>) -> Brain {
+        let mut error_values = vec![0.0; pool.len()];
+        for _i in 0..100 {
+            let expt: f64;
+            let mut rng = rand::thread_rng();
+            let mut alea: Vec<u64> = (0..pool[0].nbin).map(|_x| rng.gen_range(0..10)).collect();
+            if rng.gen::<f64>() > 0.5 {
+                alea.sort();
+            }
+            if alea.windows(2).all(|w| w[0] <= w[1]) {
+                expt = 1.0;
+            } else {
+                expt = 0.0;
+            }
+            //println!("Alea = {:?}, Expt = {:?}", alea, expt);
+            for (i, brain) in pool.iter_mut().enumerate() {
+                let l = Brain::data_scaler(&alea.iter().map(|x| *x as f64).collect());
+                let r = brain.compute(&l);
+                error_values[i] += f64::abs(expt - r[0]);
+            }
+        }
+        //get the minimum error
+        let mut min = -1.0;
+        let mut bi = 0;
+        for (i, j) in error_values.iter().enumerate() {
+            if *j < min || min == -1.0 {
+                min = *j;
+                bi = i;
+            }
+        }
+        println!("error : {}", error_values[bi]);
+        pool[bi].clone()
+    }
+
+    pub fn save(&self, path: &str) {
+        save_file(path, 0, self).unwrap()
+    }
+
+    pub fn load(path: &str) -> Brain {
+        load_file(path, 0).unwrap()
+    }
+
+    pub fn max_vf64(target: &[f64]) -> f64 {
+        let mut res = f64::NEG_INFINITY;
+        for i in target.iter() {
+            if *i > res {
+                res = *i;
+            }
+        }
+        res
+    }
+
+    pub fn min_vf64(target: &[f64]) -> f64 {
+        let mut res = f64::INFINITY;
+        for i in target.iter() {
+            if *i < res {
+                res = *i;
+            }
+        }
+        res
+    }
+
+    pub fn data_scaler(target: &Vec<f64>) -> Vec<f64> {
+        let mut res = target.clone();
+        let mut min: f64 = Brain::min_vf64(target);
+        let mut max: f64 = Brain::max_vf64(target);
+        if min != max {
+            let mut tweak = (0.0 - min) / (max + (0.0 - min));
+            if max < 0.0 {
+                min = f64::abs(min);
+                max = f64::abs(max);
+                tweak = (0.0 - min) / (max + (0.0 - min));
+                res.iter_mut().for_each(|x| {
+                    *x = -1.0 * ((f64::abs(*x) + (0.0 - min)) / (max + (0.0 - min)) - tweak)
+                });
+            } else {
+                res.iter_mut()
+                    .for_each(|x| *x = (*x + (0.0 - min)) / (max + (0.0 - min)) - tweak);
+            }
+        } else {
+            res.iter_mut().for_each(|x| {
+                if *x != 0.0 {
+                    *x = *x / f64::abs(*x);
+                } else {
+                    *x = 0.0;
+                }
+            });
+        }
+        res
+    }
+}
---
+++ brain/src/main.rs
@@ -0,0 +1,94 @@
+use brain::*;
+//use std::fs::File;
+//use std::io::Write;
+
+fn main(){
+
+    /*
+    //XOR IA TESTING
+    //let mut xorer = Brain::genetic_selection(2, 1, 3, 3,ActivationFunction::Heaviside ,Brain::learn_xor, 10000, 1000, 0.4, 0.4);
+    //xorer.save("xorer.bin");
+    let mut xorer = Brain::load("xorer.bin");
+
+    
+    let e1 = vec![0.0,0.0];
+    let e2 = vec![1.0,0.0];
+    let e3 = vec![0.0,1.0];
+    let e4 = vec![1.0,1.0];
+
+    let r1 = xorer.compute(&e1);
+    let r2 = xorer.compute(&e2);
+    let r3 = xorer.compute(&e3);
+    let r4 = xorer.compute(&e4);
+
+    //println!("{}\n",Brain::sigmoide(0.1));
+
+    println!("Xorer({:?})={:?}",e1,r1);
+    println!("Xorer({:?})={:?}",e2,r2);
+    println!("Xorer({:?})={:?}",e3,r3);
+    println!("Xorer({:?})={:?}",e4,r4);
+    */
+
+
+    /*
+    //OR IA TESTING
+    let mut orer = Brain::genetic_selection(2, 1, 2, 3,ActivationFunction::Heaviside ,Brain::learn_or, 1000, 1000, 0.4, 0.4);
+    //orer.save("orer.bin");
+    //let mut orer = Brain::load("orer.bin");
+
+    
+    let e1 = vec![0.0,0.0];
+    let e2 = vec![1.0,0.0];
+    let e3 = vec![0.0,1.0];
+    let e4 = vec![1.0,1.0];
+
+    let r1 = orer.compute(&e1);
+    let r2 = orer.compute(&e2);
+    let r3 = orer.compute(&e3);
+    let r4 = orer.compute(&e4);
+
+    //println!("{}\n",Brain::sigmoide(0.1));
+
+    println!("Orer({:?})={:?}",e1,r1);
+    println!("Orer({:?})={:?}",e2,r2);
+    println!("Orer({:?})={:?}",e3,r3);
+    println!("Orer({:?})={:?}",e4,r4);
+    */
+
+    
+    //SORTITION TESTING AI TESTS
+    //let mut sorterer = Brain::genetic_selection(8, 1, 4, 3, ActivationFunction::Heaviside, Brain::learn_sortition, 10, 10000, 0.4, 0.4);
+    let mut sorterer = Brain::load("sorterer.bin");
+    //sorterer.save("sort_master.bin");
+
+    let mut l1 = Brain::data_scaler(&vec![1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]);
+    let mut l2 = Brain::data_scaler(&vec![3.0,2.0,1.0,4.0,6.0,8.0,7.0,5.0]);
+
+    let lr1 = sorterer.compute(&mut l1);
+    let lr2 = sorterer.compute(&mut l2);
+
+    println!("Sortester({:?})={:?}",l1,lr1);
+    println!("Sortester({:?})={:?}",l2,lr2);
+    
+
+    /*
+    let data:Vec<f64> = vec![-2.0,-1.0,0.0,1.0,2.0,3.0,4.0];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+
+    let data:Vec<f64> = vec![-4.0,-3.0,-2.0,-1.0,1.0,2.0];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+
+    let data:Vec<f64> = vec![-4.0,-3.0,-2.0,-1.0,1.0,2.0,3.0,4.0];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+
+    let data:Vec<f64> = vec![0.0,0.1];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+
+    let data:Vec<f64> = vec![0.0,0.0];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+
+    let data:Vec<f64> = vec![-1.0,1.0];
+    println!("data_scaler({:?}) = {:?}", data, Brain::data_scaler(&data));
+    */
+    
+}
